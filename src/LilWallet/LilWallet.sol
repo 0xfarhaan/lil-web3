e6efb2eb67d18b321a7f65b935ac8453437246c5
farhaan-ali <farhaan@gro.xyz>
2022-07-06 13:17:15 +0100

    style: FA - 06.07.22 - Update prettier settings and run

--- src/LilWallet/LilWallet.sol
+++ src/LilWallet/LilWallet.sol
@@ -1,42 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
+import { IERC20 } from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 
 contract LilWallet {
-    using SafeERC20 for IERC20;
-    address owner;
-    bool baseImplementation;
+	using SafeERC20 for IERC20;
+	address owner;
+	bool baseImplementation;
 
-    error AlreadyInitilised();
-    error IsBaseContract();
-    error NotOwner();
+	error AlreadyInitilised();
+	error IsBaseContract();
+	error NotOwner();
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
+	event Transfer(address indexed from, address indexed to, uint256 value);
 
-    modifier onlyOwner() {
-        if (msg.sender != owner) revert NotOwner();
-        _;
-    }
+	modifier onlyOwner() {
+		if (msg.sender != owner) revert NotOwner();
+		_;
+	}
 
-    constructor() {
-        //
-        baseImplementation = true;
-    }
+	constructor() {
+		baseImplementation = true;
+	}
 
-    function initilise(address _owner) external {
-        if (baseImplementation) revert IsBaseContract();
-        if (owner != address(0)) revert AlreadyInitilised();
-        owner = _owner;
-    }
+	function initilise(address _owner) external {
+		if (baseImplementation) revert IsBaseContract();
+		if (owner != address(0)) revert AlreadyInitilised();
+		owner = _owner;
+	}
 
-    function transferToken(
-        address token,
-        address _to,
-        uint256 _amount
-    ) external onlyOwner {
-        IERC20(token).safeTransfer(_to, _amount);
-        emit Transfer(address(this), _to, _amount);
-    }
+	function transferToken(
+		address token,
+		address _to,
+		uint256 _amount
+	) external onlyOwner {
+		IERC20(token).safeTransfer(_to, _amount);
+		emit Transfer(address(this), _to, _amount);
+	}
 }
--- src/LilWallet/LilWalletFactory.sol
+++ src/LilWallet/LilWalletFactory.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {Clones} from "openzeppelin-contracts/contracts/proxy/Clones.sol";
-import {LilWallet} from "./LilWallet.sol";
+import { Clones } from 'openzeppelin-contracts/contracts/proxy/Clones.sol';
+import { LilWallet } from './LilWallet.sol';
 
 contract LilWalletFactory {
-    address baseImplementation;
+	address baseImplementation;
 
-    mapping(address => address[]) public implementations;
+	mapping(address => address[]) public implementations;
 
-    event LogNewWallet(address indexed _address, address indexed _owner);
+	event LogNewWallet(address indexed _address, address indexed _owner);
 
-    constructor(address _baseImplementation) {
-        baseImplementation = _baseImplementation;
-    }
+	constructor(address _baseImplementation) {
+		baseImplementation = _baseImplementation;
+	}
 
-    function deployWalletClone() external returns(address newImplementation) {
-        newImplementation = Clones.clone(baseImplementation);
-        implementations[msg.sender].push(newImplementation);
-        LilWallet(newImplementation).initilise(msg.sender);
+	function deployWalletClone() external returns (address newImplementation) {
+		newImplementation = Clones.clone(baseImplementation);
+		implementations[msg.sender].push(newImplementation);
+		LilWallet(newImplementation).initilise(msg.sender);
 
-        emit LogNewWallet(newImplementation, msg.sender);
-    }
+		emit LogNewWallet(newImplementation, msg.sender);
+	}
 }
